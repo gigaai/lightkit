db0e5f4fd2de0d8db6a9e6dfff3684875f3ff028
Gary Lamp <gary@giga.ai>
2020-03-17 16:35:15 +0700

    Format

--- src/Cookie.php
+++ src/Cookie.php
@@ -10,82 +10,86 @@ namespace GigaAI\LightKit;
  */
 class Cookie
 {
-	/**
-	 * Get the session value by key
-	 *
-	 * @param mixed $key
-	 * @param string $default
-	 *
-	 * @return string
-	 */
-	public static function get( $key, $default = '' ) {
+    /**
+     * Get the session value by key
+     *
+     * @param mixed $key
+     * @param string $default
+     *
+     * @return string
+     */
+    public static function get($key, $default = '')
+    {
+        if (array_key_exists($key, $_COOKIE)) {
+            $value = $_COOKIE[$key];
 
-		if ( array_key_exists( $key, $_COOKIE ) ) {
-			$value = $_COOKIE[ $key ];
+            // Value marked as flash
+            if (strpos($value, 'FLASH!!', 0) !== false) {
+                $value = substr($value, 7, strlen($value));
+                self::forget($key);
+            }
 
-			// Value marked as flash
-			if (strpos($value, 'FLASH!!', 0) !== false) {
-				$value = substr($value, 7, strlen($value));
-				self::forget($key);
-			}
+            return $value;
+        }
 
-			return $value;
-		}
+        return $default;
+    }
 
-		return $default;
-	}
-
-	/**
-	 * Check if the session has given key
-	 *
-	 * @param $key
-	 *
-	 * @return bool
-	 */
-	public static function has( $key ) {
+    /**
+     * Check if the session has given key
+     *
+     * @param $key
+     *
+     * @return bool
+     */
+    public static function has($key)
+    {
         return array_key_exists($key, $_COOKIE);
-	}
+    }
 
-	/**
-	 * Set the session value
-	 *
-	 * @param        $key
-	 * @param string $value
-	 */
-	public static function set( $key, $value = '' ) {
-		if ( ! is_array( $key ) ) {
+    /**
+     * Set the session value
+     *
+     * @param        $key
+     * @param string $value
+     */
+    public static function set($key, $value = '')
+    {
+        if (!is_array($key)) {
             $key = [$key => $value];
         }
 
-        foreach ( $key as $k => $v ) {
+        foreach ($key as $k => $v) {
             setcookie($k, $v, time() + 3600, COOKIEPATH, COOKIE_DOMAIN);
         }
-	}
+    }
 
-	/**
-	 * Remove session key
-	 *
-	 * @param $key
-	 */
-	public static function forget( $key ) {
-        unset( $_COOKIE[$key] );
+    /**
+     * Remove session key
+     *
+     * @param $key
+     */
+    public static function forget($key)
+    {
+        unset($_COOKIE[$key]);
         setcookie($key, '', time() - 3600, COOKIEPATH, COOKIE_DOMAIN);
-	}
+    }
 
-	/**
-	 * Session flash. Generate one time session value.
-	 *
-	 * @param        $key
-	 * @param string $value
-	 */
-	public static function flash( $key, $value = '' ) {
+    /**
+     * Session flash. Generate one time session value.
+     *
+     * @param        $key
+     * @param string $value
+     */
+    public static function flash($key, $value = '')
+    {
 
-		if ( ! is_array( $key ) ) {
-           $key = [$key => $value];
+        if (!is_array($key)) {
+            $key = [$key => $value];
         }
 
-        foreach ( $key as $k => $v ) {
+        foreach ($key as $k => $v) {
             self::set($k, 'FLASH!!' . $v);
-		}
-	}
+        }
+    }
 }
--- src/Setting.php
+++ src/Setting.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace GigaAI\LightKit;
 
 class Setting
